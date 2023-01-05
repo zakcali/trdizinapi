3aaf4d57d6bc3fd88643c2c594c0826bb927fce3
Zafer Akçalı <zaferakcali@yahoo.com>
2023-01-05 23:52:48 +0300

    Add files via upload

---
+++ JsonTrPublication.php
@@ -0,0 +1,125 @@
+<?php
+class getJsonTrPublication {
+	public $trdizinid='', $doi='', $ArticleTitle='', $dergi='', $ISOAbbreviation='', $ISSN='', $eISSN='', $Year='', $Volume='', $Issue='', $StartPage='', $EndPage='', $yazarlar='', $PublicationType='', $AbstractText='', $dergiLinki='', $ArticleType='', $dikkat='';
+	public $yazarS=0;
+
+	    function __construct() {
+		}
+		
+		final function trPublication ($numara) {
+			
+		$preText="https://search.trdizin.gov.tr/yayin/detay/";
+		$url = $preText.$numara.'?view=json';
+	$headers = get_headers($url);
+	if(substr($headers[0], 9, 3) != "200")
+		$this->dikkat='yayın bulunamadı';    
+	else { // 404 hatası gelmedi, demek ki yayın sayfası var.
+
+	$icerik=@file_get_contents($url);
+//	echo $icerik;
+// On Windows, uncomment the following line in php.ini, and restart the Apache server:
+// extension=mbstring
+// extension=php_mbstring.dll
+	$html= mb_convert_encoding($icerik, 'HTML-ENTITIES', "UTF-8");
+	$cisim1=json_decode ($html);
+	$cisim=$cisim1->records[0];
+//	print_r($cisim);
+// trdizin numarası
+			if (isset ($cisim->id)) 
+				$this->trdizinid=$cisim->id;
+// Makalenin başlığı
+			if (isset ($cisim->title)) 
+				$this->ArticleTitle = $cisim->title;
+// yayın türü
+			if (isset ($cisim->docType))
+				$this->PublicationType = $cisim->docType;
+				if ($cisim->docType == 'paper')
+					$this->PublicationType ='Makale';
+				else if ($cisim->docType == 'project')
+					$this->PublicationType ='Proje';
+
+// makale türü
+			if (isset ($cisim->publicationType)) {
+				$this->ArticleType = $cisim->publicationType;
+				if ($cisim->publicationType == 'RESEARCH')
+					$this->ArticleType = 'Araştırma Makalesi';
+				else if ($cisim->publicationType == 'LETTER_TO_EDITOR')
+					$this->ArticleType = 'Editöre Mektup';
+				else if ($cisim->publicationType == 'FACT_PRESENTATION')
+					$this->ArticleType = 'Olgu Sunumu';
+				else if ($cisim->publicationType == 'COMPILATION')
+					$this->ArticleType = 'Derleme';
+				else if ($cisim->publicationType == 'BOOK_PRESENTATION')
+					$this->ArticleType = 'Kitap Tanıtımı';
+				else if ($cisim->publicationType == 'TRANSLATION')
+					$this->ArticleType = 'Çeviri';
+				else if ($cisim->publicationType == 'EDITORIAL')
+					$this->ArticleType = 'Editoryal';
+				else if ($cisim->publicationType == 'LETTER')
+					$this->ArticleType = 'Mektup';
+				else if ($cisim->publicationType == 'REPORT')
+					$this->ArticleType = 'Bildiri';
+				else if ($cisim->publicationType == 'SHORT_REPORT')
+					$this->ArticleType = 'Kısa Bildiri';
+				else if ($cisim->publicationType == 'MEETING_SUMMARY')
+					$this->ArticleType = 'Toplantı Özetleri';
+				else if ($cisim->publicationType == 'CORRECTION')
+					$this->ArticleType = 'Düzeltme';
+				else if ($cisim->publicationType == 'OTHER')
+					$this->ArticleType = 'Diğer';
+			}
+// Özet
+			if (isset ($cisim->abstract))
+				$this->AbstractText = $cisim->abstract;
+				
+// doi
+			if (isset ($cisim->publicationNumber))
+				$this->doi = $cisim->publicationNumber;
+				
+// Dergi ismi
+			if (isset ($cisim->journalName))
+				$this->dergi = $cisim->journalName;
+// Dergi linki
+			if (isset ($cisim->journalCode))
+				$this->dergiLinki = $cisim->journalCode;
+	
+// Aldığı atıf sayısı
+// dergi kısa ismi
+// PMID
+
+// issn
+			if (isset ($cisim->issn)) 
+				$this->ISSN = $cisim->issn;
+// issn
+			if (isset ($cisim->eissn))
+				$this->eISSN = $cisim->eissn;
+				
+//Yıl
+			if (isset ($cisim->issue->issue_year))
+				$this->Year=$cisim->issue->issue_year;
+// cilt
+			if (isset ($cisim->issue->issueVolume))
+				$this->Volume=$cisim->issue->issueVolume;
+// sayı
+			if (isset ($cisim->issue->issueNumber))
+				$this->Issue=$cisim->issue->issueNumber;
+// başlangıç sayfası
+			if (isset ($cisim->startPage))
+				$this->StartPage = $cisim->startPage;
+// bitiş sayfası
+			if (isset ($cisim->endPage))
+				$this->EndPage = $cisim->endPage;
+// yazar sayısı
+			$this->yazarS=0;
+// yazarlar
+			$this->yazarlar="";
+
+			foreach ($cisim->author as $yazar) {
+				$this->yazarlar=$this->yazarlar.$yazar->authorNameInPaper.", ";
+				$this->yazarS=$this->yazarS+1;
+					}
+			$this->yazarlar=substr ($this->yazarlar,0,-2);
+		} // icerik geldi ,işlendi
+	} // final function trPublication
+
+}
---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<!-- trdizinapi V1.0: bu yazılım Dr. Zafer Akçalı tarafından oluşturulmuştur 
+programmed by Zafer Akçalı, MD -->
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Trdizin numarasından makaleyi bul</title>
+</head>
+
+<body>
+<?php
+// By Zafer Akçalı, MD
+// Zafer Akçalı tarafından programlanmıştır
+require_once 'JsonTrPublication.php';
+$tr=new getJsonTrPublication ();
+
+if (isset($_POST['trdizinid'])) {
+$gelenTrdizin=preg_replace("/[^0-9]/", "", $_POST["trdizinid"] ); // sadece rakamlar
+$tr->trPublication ($gelenTrdizin);
+}
+?>
+<a href="trdizin id nerede.png" target="_blank"> Trdizin numarasına nereden bakılır? </a>
+<form method="post" action="">
+Trdizin makale numarasını giriniz. <?php echo ' '.$tr->dikkat;?><br/>
+<input type="text" name="trdizinid" id="trdizinid" value="<?php echo $tr->trdizinid;?>" >
+<input type="submit" value="Trdizin yayın bilgilerini PHP ile getir">
+</form>
+<button id="trDizinGoster" onclick="trDizinGoster()">Trdizin yayınını göster</button>
+<button id="trDizinAtifGoster" onclick="trDizinAtifGoster()">Trdizin yayınının atıflarını göster</button>
+<button id="doiGit" onclick="doiGit()">doi ile makaleyi göster</button>
+<button id="dergiGit" onclick="dergiGit()">Dergiyi gör</button>
+<br/>
+Trdizin id: <input type="text" name="eid" size="25" id="eid" value="<?php echo $tr->trdizinid;?>" >  
+doi: <input type="text" name="doi" size="55"  id="doi" value="<?php echo $tr->doi;?>"> <br/>
+Başlık: <input type="text" name="ArticleTitle" size="96"  id="ArticleTitle" value="<?php echo str_replace ('"',  '&#34',$tr->ArticleTitle);?>"> <br/>
+Dergi ismi: <input type="text" name="Title" size="50"  id="Title" value="<?php echo $tr->dergi;?>"> <br/>
+ISSN: <input type="text" name="ISSN" size="8"  id="ISSN" value="<?php echo $tr->ISSN;?>">
+eISSN: <input type="text" name="eISSN" size="8"  id="eISSN" value="<?php echo $tr->eISSN;?>">
+<br/>
+Yıl: <input type="text" name="Year" size="4"  id="Year" value="<?php echo $tr->Year;?>">
+Cilt: <input type="text" name="Volume" size="2"  id="Volume" value="<?php echo $tr->Volume;?>">
+Sayı: <input type="text" name="Issue" size="2"  id="Issue" value="<?php echo $tr->Issue;?>">
+Sayfa/numara: <input type="text" name="StartPage" size="5"  id="StartPage" value="<?php echo $tr->StartPage;?>">
+- <input type="text" name="EndPage" size="2"  id="EndPage" value="<?php echo $tr->EndPage;?>">
+Yazar sayısı: <input type="text" name="yazarS" size="2"  id="yazarS" value="<?php echo $tr->yazarS;?>"><br/>
+Yazarlar: <input type="text" name="yazarlar" size="95"  id="yazarlar" value="<?php echo $tr->yazarlar;?>"><br/>
+Yayın türü: <input type="text" name="PublicationType" size="10"  id="PublicationType" value="<?php echo $tr->PublicationType;?>">
+Makale türü: <input type="text" name="ArticleType" size="15"  id="ArticleType" value="<?php echo $tr->ArticleType;?>">
+Dergi numarası: <input type="text" name="dergiLinki" size="10"  id="dergiLinki" value="<?php echo $tr->dergiLinki;?>">
+<br/>
+Özet <br/>
+<textarea rows = "20" cols = "90" name = "ozet" id="ozetAlan"><?php echo $tr->AbstractText;?></textarea>  <br/>
+<script>
+function trDizinGoster() {
+var	w=document.getElementById('eid').value.replace('2-s2.0-','');
+	urlText = "https://search.trdizin.gov.tr/yayin/detay/" + w;
+	window.open(urlText,"_blank");
+}
+function trDizinAtifGoster() {
+var	w=document.getElementById('eid').value;
+	urlText = "https://search.trdizin.gov.tr/yayin/detay/"+w+"/#publications";
+	window.open(urlText,"_blank");
+}
+function doiGit() {
+var	w=document.getElementById('doi').value;
+	urlText = "https://doi.org/"+w;
+	window.open(urlText,"_blank");
+}
+function dergiGit() {
+var	w=document.getElementById('dergiLinki').value;
+	urlText = 'https://search.trdizin.gov.tr/dergi/detay/'+w;
+	window.open(urlText,"_blank");
+}
+</script>
+</body>
+</html>
