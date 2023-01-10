8f84c6ec49a27339907aced5efd8aef9186b6b7d
Zafer Akçalı <zaferakcali@yahoo.com>
2023-01-10 12:10:51 +0300

    class, url error control

---
+++ python/getTrDizinPublication.py
@@ -0,0 +1,110 @@
+# trdizinapi V1.0, by Zafer Akçalı
+
+import urllib.request as req
+from urllib.error import URLError, HTTPError
+import certifi
+import ssl
+import json
+
+class getTrDizinPublication: 
+    def __init__(self):
+        self.yazarlar=''; self.trdizinid=''; self.doi=''; self.ArticleTitle=''; self.dergi=''; self.ISOAbbreviation=''; self.ISSN=''; self.eISSN=''; self.Year=''; self.Volume=''; self.Issue=''; self.StartPage=''; self.EndPage=''; self.PublicationType=''; self.AbstractText=''; self.dergiLinki=''; self.ArticleType=''; self.dikkat=''; 
+        self.yazarS=0
+    
+    def trDizinPublication (self,numara):
+        preText = "https://search.trdizin.gov.tr/yayin/detay/"
+        postText = "?view=json"
+        url = preText+numara+postText
+        #print("hedef:", url)
+# https://stackoverflow.com/questions/27835619/urllib-and-ssl-certificate-verify-failed-error
+        try:
+            response = req.urlopen(url, context=ssl.create_default_context(cafile=certifi.where()))
+        except HTTPError as e:
+            self.dikkat="yayın bulunamadı"
+            return
+        except URLError as e:
+            self.dikkat="bağlantı kurulamadı"
+            return
+        self.dikkat="yayın bulundu"
+        
+        bytecode = response.read ()
+        htmlstr = bytecode.decode().replace ("\\r","") # carriage return \r karakterlerini çıkart
+# print (htmlstr)
+        trDic=json.loads (htmlstr) 
+        for key,value in trDic['records'][0].items():
+#    print("The key and value are {} = {}".format(key, value))
+#    print ()
+            if key == 'title':
+                self.ArticleTitle = value 
+            elif key == 'id':
+                self.trdizinid = value 
+            elif key == 'abstract':
+                self.AbstractText = value 
+            elif key == 'publicationNumber':
+                self.doi = value            
+            elif key == 'journalName':
+                self.dergi = value  
+            elif key == 'journalCode':
+                self.dergiLinki = value 
+            elif key == 'issn':
+                self.ISSN = value             
+            elif key == 'eissn':
+                self.eISSN = value   
+            elif key == 'startPage':
+                self.StartPage = value
+            elif key == 'endPage':
+                self.EndPage = value
+
+            elif key == 'docType':
+                if value == "paper":
+                    self.PublicationType = "Makale"
+                elif value == "project":
+                    self.PublicationType = "Proje"
+                elif value:
+                    self.PublicationType = value
+            elif key == 'publicationType':
+                if value == "RESEARCH":
+                    self.ArticleType = "Araştırma Makalesi"
+                elif value == "LETTER_TO_EDITOR":
+                    self.ArticleType = "Editöre Mektup"
+                elif value == "FACT_PRESENTATION":
+                    self.ArticleType = "Olgu Sunumu"
+                elif value == "COMPILATION":
+                    self.ArticleType = "Derleme"
+                elif value == "BOOK_PRESENTATION":
+                    self.ArticleType = "Kitap Tanıtımı"
+                elif value == "TRANSLATION":
+                    self.ArticleType = "Çeviri"
+                elif value == "EDITORIAL":
+                    self.ArticleType = "Editoryal"
+                elif value == "LETTER":
+                    self.ArticleType = "Mektup"
+                elif value == "REPORT":
+                    self.ArticleType = "Bildiri"
+                elif value == "SHORT_REPORT":
+                    self.ArticleType = "Kısa Bildiri"
+                elif value == "MEETING_SUMMARY":
+                    self.ArticleType = "Toplantı Özetleri"
+                elif value == "CORRECTION":
+                    self.ArticleType = "Düzeltme"
+                elif value == "OTHER":
+                    self.ArticleType = "Diğer"
+                elif value:
+                    self.ArticleType = value
+ 
+            elif key == "issue":
+                for key2, value2 in value.items():
+                    if key2 == "issue_year":
+                        self.Year = value2   
+                    elif key2 == "issueVolume":
+                        self.Volume = value2
+                    elif key2 == "issueNumber":
+                        self.Issue = value2     
+            elif key == "author":
+                authors = "";
+                for i in range (len(value)):
+                    for key3, value3 in value[i].items():
+                        if key3 == "authorNameInPaper":
+                            authors = authors+ value3 + ", "
+                            self.yazarS=self.yazarS+1
+                self.yazarlar = authors[:-2]
--- python/main.py
+++ python/main.py
@@ -1,92 +1,24 @@
-import urllib.request as req
-import json
-yazarlar="";
-yazarS=0;
-preText = "https://search.trdizin.gov.tr/yayin/detay/"
-postText = "?view=json"
-numara = input("trdizin numarasını giriniz:")
-url = preText+numara+postText
-print("hedef:", url)
-response = req.urlopen(url)
-bytecode = response.read ()
-htmlstr = bytecode.decode().replace ("\\r","") # carriage return \r karakterlerini çıkart
-# print (htmlstr)
-trDic=json.loads (htmlstr) 
-for key,value in trDic['records'][0].items():
-#    print("The key and value are {} = {}".format(key, value))
-#    print ()
-    if key == 'title':
-       print ("ArticleTitle = ", (value)) 
-    elif key == 'id':
-        print ("trdizinid = ", (value))
-    elif key == 'docType':
-        if value == "paper":
-            print ("PublicationType = "+"Makale")
-        elif value == "project":
-            print ("PublicationType = "+"Proje")
-        elif value:
-            print ("PublicationType = ", (value))
-    elif key == 'publicationType':
-        if value == "RESEARCH":
-            print ("ArticleType = "+"Araştırma Makalesi")
-        elif value == "LETTER_TO_EDITOR":
-            print ("ArticleType = "+"Editöre Mektup")
-        elif value == "FACT_PRESENTATION":
-            print ("ArticleType = "+"Olgu Sunumu")
-        elif value == "COMPILATION":
-            print ("ArticleType = "+"Derleme")
-        elif value == "BOOK_PRESENTATION":
-            print ("ArticleType = "+"Kitap Tanıtımı")
-        elif value == "TRANSLATION":
-            print ("ArticleType = "+"Çeviri")
-        elif value == "EDITORIAL":
-            print ("ArticleType = "+"Editoryal")
-        elif value == "LETTER":
-            print ("ArticleType = "+"Mektup")
-        elif value == "REPORT":
-            print ("ArticleType = "+"Bildiri")
-        elif value == "SHORT_REPORT":
-            print ("ArticleType = "+"Kısa Bildiri")
-        elif value == "MEETING_SUMMARY":
-            print ("ArticleType = "+"Toplantı Özetleri")
-        elif value == "CORRECTION":
-            print ("ArticleType = "+"Düzeltme")
-        elif value == "OTHER":
-            print ("ArticleType = "+"Diğer")
-        elif value:
-            print ("ArticleType = ", (value))
-    elif key == 'abstract':
-        print ("AbstractText = ", (value))
-    elif key == 'publicationNumber':
-        print ("doi = ", (value))
-    elif key == 'journalName':
-        print ("dergi = ", (value))
-    elif key == 'journalCode':
-        print ("dergiLinki = ", (value))
-    elif key == 'issn':
-        print ("ISSN = ", (value))
-    elif key == 'eissn':
-        print ("eISSN = ", (value))
-    elif key == 'startPage':
-        print ("StartPage = ", (value))
-    elif key == 'endPage':
-        print ("EndPage = ", (value))
-        
-    elif key == "issue":
-        for key2, value2 in value.items():
-            if key2 == "issue_year":
-                print ("Year = ", (value2))   
-            elif key2 == "issueVolume":
-                print ("Volume = ", (value2))
-            elif key2 == "issueNumber":
-                print ("Issue = ", (value2))     
-    elif key == "author":
-        authors = "";
-        for i in range (len(value)):
-            for key3, value3 in value[i].items():
-                if key3 == "authorNameInPaper":
-                    authors = authors+ value3 + ", "
-                    yazarS=yazarS+1
-        yazarlar = authors[:-2]
-        print ("Yazarlar = ", (yazarlar))
-        print ("Yazar sayısı = ", (yazarS))
+from getTrDizinPublication import getTrDizinPublication
+
+trDizinNumara = input("trdizin numarasını giriniz:")
+
+tr = getTrDizinPublication ()
+tr.trDizinPublication (trDizinNumara)
+print ("", (tr.dikkat))
+print ("Trdizin id: ", (tr.trdizinid))
+print ("doi: ", (tr.doi))
+print ("Başlık: ", (tr.ArticleTitle)) 
+print ("Dergi ismi: ", (tr.dergi))
+print ("ISSN: ", (tr.ISSN))
+print ("eISSN: ", (tr.eISSN))
+print ("Yıl: ", (tr.Year))
+print ("Cilt: ", (tr.Volume))
+print ("Sayı: ", (tr.Issue))
+print ("Sayfa/numara: ", (tr.StartPage))
+print ("Sayfa/numara bitiş: ", (tr.EndPage))
+print ("Yazar sayısı: ", (tr.yazarS))
+print ("Yazar(lar): ", (tr.yazarlar))
+print ("Dergi numarası: ", (tr.dergiLinki))
+print ("Yayın türü: ", (tr.PublicationType))
+print ("Makale türü: ", (tr.ArticleType))
+print ("Özet: ", (tr.AbstractText))
